1ffa8f21d76a196f4889290b8a0d670992cbb146
edu <edu@gmail.com>
2026-08-03 15:46:58 +0530

    build issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    port: 8080,
+    host: true,
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
   plugins: [
     tsconfigPaths(),
-    tanstackStart({
-      server: { entry: "server" },
-    }),
     tailwindcss(),
     react(),
   ],
